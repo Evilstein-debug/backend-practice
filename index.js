a9f7b4b18598aa3c1d7497cf06424f73f35e85ce
evilstein-debug <tejaspathak2005@gmail.com>
2025-06-28 01:49:22 +0530

    sever init

---
+++ index.js
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import express from "express"
+
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send("hello you are on my server now!")
+})
+
+app.get('/x', (req,res) => {
+    res.send("@evilstein_")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`now listening on port ${process.env.PORT}`)
+})
