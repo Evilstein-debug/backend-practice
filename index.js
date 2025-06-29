3df7f0af009431a8f5c4b377aa499325875d9573
evilstein-debug <tejaspathak2005@gmail.com>
2025-06-29 17:56:48 +0530

    cors and proxy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import express from "express"
+import { title } from 'process'
 
 const app = express()
 
@@ -9,6 +10,42 @@ app.get('/', (req,res) => {
 
 app.get('/x', (req,res) => {
     res.send("@evilstein_")
+})
+
+app.get('/api/jokes', (req,res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: 'A first joke',
+            content: 'This is the first joke'
+
+        },
+        {
+            id: 2,
+            title: 'A second joke',
+            content: 'This is the second joke'
+
+        },
+        {
+            id: 3,
+            title: 'A third joke',
+            content: 'This is the third joke'
+
+        },
+        {
+            id: 4,
+            title: 'A fourth joke',
+            content: 'This is the fourth joke'
+
+        },
+        {
+            id: 5,
+            title: 'A fifth joke',
+            content: 'This is the fifth joke'
+
+        }
+    ]
+    res.send(jokes)
 })
 
 app.listen(process.env.PORT, () => {
