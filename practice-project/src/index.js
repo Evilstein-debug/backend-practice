e8504d1eaff17efd0483f3076186083ad8265b8a
evilstein-debug <tejaspathak2005@gmail.com>
2025-07-04 12:42:23 +0530

    start express server after connecting to DB

--- practice-project/src/index.js
+++ practice-project/src/index.js
@@ -1,7 +1,22 @@
 import 'dotenv/config'
 import connectDB from "./db/index.js";
+import app from './app.js';
+
+const port = process.env.PORT || 8000
 
 connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Error in express application: ", error)
+        throw error
+    })
+    app.listen(port, () => {
+        console.log(`app listening on port: ${port}`)
+    })
+})
+.catch((error) => {
+    console.log("MongoDB connection error!", error)
+})
 
 
 
