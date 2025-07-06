2f4a08a16f00d02f2d87a577f7fb0b8b5d4858c7
evilstein-debug <tejaspathak2005@gmail.com>
2025-07-07 00:15:24 +0530

    setup handler

--- practice-project/src/app.js
+++ practice-project/src/app.js
@@ -1,5 +1,24 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN, //cors origin currently set to * but in real use cases we set it to our fronted url.
+    credentials: true
+}))
+
+app.use(express.json({
+    limit: "16kb"
+}))
+
+app.use(express.urlencoded({
+    extended: true,
+    limit: "16kb"
+}))
+
+app.use(express.static("public"))
+app.use(cookieParser())
 
 export default app
