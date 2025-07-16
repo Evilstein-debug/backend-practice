16148b4a25ba18f4a509adde1ffff39b87c4d3b8
evilstein-debug <tejaspathak2005@gmail.com>
2025-07-16 18:18:24 +0530

    api error and response

---
+++ practice-project/src/utils/apiResponse.js
@@ -0,0 +1,8 @@
+class apiResponse{
+    constructor(message="Success", statusCode, data){
+        this.message = message
+        this.statusCode = statusCode < 400
+        this.data = data
+
+    }
+}
