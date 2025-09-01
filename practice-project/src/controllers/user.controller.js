87f61e33226da3981df2091df5dc09b1f982572d
evilstein-debug <tejaspathak2005@gmail.com>
2025-09-01 20:57:25 +0530

    handled empty coverImage error bug and unlink local file after upload

--- practice-project/src/controllers/user.controller.js
+++ practice-project/src/controllers/user.controller.js
@@ -6,7 +6,6 @@ import { apiResponse } from "../utils/apiResponse.js";
 
 const registerUser = asyncHandler(async(req, res) => {
     const {email, fullname, username, password} = req.body
-    console.log(email);
 
     if(
         [fullname, email, password, username].some((field) => field?.trim() === "")
@@ -21,15 +20,19 @@ const registerUser = asyncHandler(async(req, res) => {
     }
 
 
-    const existingUser = User.find({
+    const existingUser = await User.findOne({     //can't use User.find() here
         $or: [{ email }, { username }]
     })
     if(existingUser) {
         throw new apiError(409, "User already exists!")
     }
 
-    const avatarLocalPath = req.files?.avatar[0]?.path //try changing to file
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
     if(!avatarLocalPath) {
         throw new apiError(400, "Please upload an avatar image!")
     }
@@ -54,7 +57,7 @@ const registerUser = asyncHandler(async(req, res) => {
         throw new apiError(500, "Something went wrong while registering the user!")
     }
 
-    return res.send(201).json(
+    return res.status(201).json(
         new apiResponse("User successfully registered.", 200, userCreated)
     )
 
